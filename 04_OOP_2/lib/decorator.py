6a78aa15400e1c31d02880bea5e0a4221938bad9
Guillermo <vital_tmv@yahoo.com>
2024-07-17 13:17:53 -0500

    updated OOP_2 notes

--- 04_OOP_2/lib/decorator.py
+++ 04_OOP_2/lib/decorator.py
@@ -5,9 +5,38 @@
 
     # Create functions to be used as callbacks 
 
+#First Class functions
+
+# def walk(pet):
+#     print(f'{pet} has been walked!')
+
+# def feed(pet):
+#     print(f'{pet} had been fed!')
+
+
     # Create a higher-order function that will take a callback as an argument
+#Higher order function => accepts a function
+# def execute_task(func):
+
+#     #Callback function Invocation
+#     return func("Rose")
+
+
+# execute_task(walk)
+# execute_task(feed)
 
 # 2. ✅ Create a higher-order function that returns a function
+
+# def execute_task():
+#     def feed (pet):
+#         print(f'{pet} has been fed!')
+    
+#     return feed
+
+# print(execute_task()) # returns the feed funciton reference
+
+#if we end with a fucntion reference then we can run this code below
+# execute_task()("Rose")
 
 # 3. ✅ Decorator
 
@@ -18,14 +47,44 @@
 
 # Tools:
 
-    # .format()
+    # .format() => Method (String)
     # https://www.geeksforgeeks.org/python-string-format-method/
 
-    # .round()
+    # .round() => Format Actual Calclation of Discount
     # https://www.geeksforgeeks.org/round-function-python/
+
+#Decorator
+def coupon_calculator(func):
+
+    #Inner Function
+    def report_price():
+        print(f'Initioal Price = $35.00')
+        final_price = func(35.00)
+        print(f'Newly Discounted Price = ${final_price}')
+
+    return report_price
+
+
+#Callback function to calculate new price
+# def calculate_price(price):
+
+#     # we end up with a floating point number rounded to the newarest hundreth
+#     return '{:.2f}'.format(round( price / 2, 2))
+
+# report_price = coupon_calculator(calculate_price)
+# report_price()
 
 # Try using a decorator with / without pie syntax '@'
 
 # Without pie syntax 
 
 # With pie syntax
+
+@coupon_calculator
+def calculate_price(price):
+
+    # we end up with a floating point number rounded to the newarest hundreth
+    return '{:.2f}'.format(round( price / 2, 2))
+
+
+calculate_price()
